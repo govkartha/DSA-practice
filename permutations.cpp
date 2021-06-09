a0672761c327d8115dceeffa084421a006900a96
Govind Kartha <44610432+govkartha@users.noreply.github.com>
2021-06-09 16:45:02 +0530

    CSES-5

--- permutations.cpp
+++ permutations.cpp
@@ -7,9 +7,18 @@ int main(){
     if(n==1) cout << '1' << endl;
     else if(n==2 || n==3) cout << "NO SOLUTION" << endl;
     else{
-        for(int i=1; i<=n; i+=2)
-            cout << i << " ";
-        for(int j=2; j<=n-1; j+=2)
-            cout << j << " ";
+        if(n%2==0){
+            for(int i=2; i<=n; i+=2)
+                cout << i << " ";
+            for(int j=1; j<=n-1; j+=2)
+                cout << j << " ";
+        }
+        else{
+            for(int i=1; i<=n; i+=2)
+                cout << i << " ";
+            for(int j=2; j<=n-1; j+=2)
+                cout << j << " ";
+        }
+        
     }
 }
